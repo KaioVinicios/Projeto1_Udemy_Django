69762845423c20d317bb47bbc93edd9a091490d6
Kaio Vinícios <kvsgpro@outlook.com>
2023-08-17 17:45:46 -0300

    Modularizando URLs responses (App)

---
+++ recipes/urls.py
@@ -0,0 +1,13 @@
+
+
+from django.urls import path
+
+from recipes.views import home,contato,sobre
+
+
+
+urlpatterns = [
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/',contato)
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Home 2')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
