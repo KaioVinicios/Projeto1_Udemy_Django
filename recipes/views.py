c28d6e4251adf541ec6dc33656e9cf0c92f04ae9
Kaio Vinícios <kvsgpro@outlook.com>
2023-11-04 10:33:52 -0300

    Alteração parcial página individual da receita.

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,6 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/recipe-view.html')
 
 
