16887956d8fd4b0959201025ee0c9e362d595db1
Kaio Vinícios <kvsgpro@outlook.com>
2023-11-04 10:19:51 -0300

    Nova URL para as receitas.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 
 
 from django.urls import path
-from recipes.views import home
+from recipes import views
 
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/',views.recipe)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html')
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html')
+
 
